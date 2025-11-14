e169da993660aa6e76ca73edb4e6a6abf8181580
Arnav Kawatra <kawatraarnav@gmail.com>
2025-11-14 16:34:28 +0530

    prod minor fix

--- build-scripts/build-prod.sh
+++ build-scripts/build-prod.sh
@@ -1,4 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
+
+if [ -f ./configs/.production.env ]; then
+  set -o allexport
+  source ./configs/.production.env
+  set +o allexport
+fi
+
 tsc
 mkdir -p ./dist/configs/
-cp ./configs/.production.env ./dist/configs/
+if [ -f ./configs/.production.env ]; then
+  cp ./configs/.production.env ./dist/configs/
+fi
